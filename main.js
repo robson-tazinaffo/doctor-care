007d06b14a434a83c27c4c15d103e55091929ac3
robson-tazinaffo <robsontazinaffo@gmail.com>
2022-05-07 03:43:55 -0300

    Eventos no scroll e personalização visual

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
@@ -13,13 +29,6 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
-
-// function ScrollReveal() {
-//   var options = {
-//     reveal: function() {}
-//   }
-//   return options
-// }
 
 ScrollReveal({
   origin: 'top',
